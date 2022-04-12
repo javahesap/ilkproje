6439efc01e559f40b2c331066b8a6ac0b9d173ab
alieryaman <postauyelik@gmail.com>
2022-04-12 15:46:17 +0300

    Initial commit

---
+++ src/ana.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ana {
+    public static void main(String[] args) {
+        int f=0,k,t,ta,m,o;
+        Scanner deger=new Scanner(System.in);
+        f=deger.nextInt();
+        t=deger.nextInt();
+        ta=deger.nextInt();
+        m=deger.nextInt();
+        k=deger.nextInt();
+       o= (f+k+t+ta+m)/5;
+
+
+        System.out.println(o);
+    }
+}
